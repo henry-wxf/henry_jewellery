f2620eca9b2119bcdde182c0f4aa97da66b8bba4
Henry Wang <13520480594@163.com>
2016-01-05 10:51:55 +1100

    add test cases

--- src/test/java/com/henry/jewellery/HenryJewelleryApplicationTests.java
+++ src/test/java/com/henry/jewellery/HenryJewelleryApplicationTests.java
@@ -1,18 +1,47 @@
 package com.henry.jewellery;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = HenryJewelleryApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest(randomPort = true)
 public class HenryJewelleryApplicationTests {
+    @Value("${local.server.port}")
+    private int port;
 
-	@Test
-	public void contextLoads() {
-	}
+    private RestTemplate template = new TestRestTemplate();
 
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void homePageLoads() {
+        ResponseEntity<String> response = template.getForEntity("http://localhost:" + port + "/", String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void loginEndpointProtected() {
+        ResponseEntity<String> response = template.getForEntity("http://localhost:" + port + "/accounts/me", String.class);
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    }
+
+    @Test
+    public void loginSucceeds() {
+        RestTemplate template = new TestRestTemplate("user", "p");
+        ResponseEntity<String> response = template.getForEntity("http://localhost:" + port + "/accounts/me", String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
 }
